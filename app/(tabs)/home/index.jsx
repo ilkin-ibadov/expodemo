1a019155be1018b5590c171ffc053619df35e28e
Ilkin Ibadov <ilkinibadov@Ilkins-MacBook-Air.local>
2025-02-23 09:32:57 +0400

    env added

--- app/(tabs)/home/index.jsx
+++ app/(tabs)/home/index.jsx
@@ -1,7 +1,32 @@
 import { View, Text } from "react-native"
 import { Link } from "expo-router"
+import { useEffect, useState } from 'react'
+import Constants from "expo-constants"
+
+const IP_URL = Constants.expoConfig.extra.IP_URL
+const ACCESS_TOKEN = Constants.expoConfig.extra.ACCESS_TOKEN
 
 const Index = () => {
+    const [movies, setMovies] = useState([])
+
+    const getMovies = async () => {
+        try {
+            const response = await fetch(`${IP_URL}/movie/trending`)
+            const data = await response.json()
+
+            setMovies(data.content)
+
+            console.log(data.content)
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        getMovies()
+    }, [])
+
     return (
         <View><Text>
             Home
--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,16 +1,38 @@
-import { Text, View } from 'react-native'
-import React from 'react'
-import Logo from "../../assets/icons/logo.svg"
+import { View, Text } from "react-native"
+import { Link } from "expo-router"
+import { useEffect, useState } from 'react'
+import Constants from "expo-constants"
 
-const index = () => {
+const IP_URL = Constants.expoConfig.extra.IP_URL
+const ACCESS_TOKEN = Constants.expoConfig.extra.ACCESS_TOKEN
+
+const Index = () => {
+    const [movies, setMovies] = useState([])
+
+    const getMovies = async () => {
+        try {
+            const response = await fetch(`${IP_URL}/movie/trending`)
+            const data = await response.json()
+
+            setMovies(data.content)
+
+            console.log(data.content)
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        getMovies()
+    }, [])
 
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: "center" }}>
-            <Logo />
-            <Text className='text-[32px] font-montserratLightItalic'>Homepage</Text>
-        </View>
+        <View><Text>
+            Home
+            <Link href="/test">Go to Homepage</Link>
+        </Text></View>
     )
-
 }
 
-export default index
+export default Index
